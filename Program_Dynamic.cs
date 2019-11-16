bc510cf4b8c68bd1f1b37a8d88d7a5c688b3dbe8
Yves Wienecke <30377629+iyves@users.noreply.github.com>
2019-11-16 00:26:01 -0800

    Add documentation, separate query into separate function, call functions from main

--- Program_Dynamic.cs
+++ Program_Dynamic.cs
@@ -31,39 +31,59 @@ namespace CS488
 
         static void Main(string[] args)
         {
-            gcp_connect(); // Warning bc I am not awaiting this function, but it's ok bc I'm not doing concurrency rn
-            Console.ReadLine();
-        }
+            // Don't forget to whitelist your ip!
+            string connection_string = "mongodb+srv://username:password@cluster_ip/test?retryWrites=true&w=majority";
+            string db_name = "airbnb";
+            bool do_import = false;
+            
+            // Wait until connection completes before proceeding, and then create db if it doesn't already exist
+            MongoClient client = new MongoClient(connection_string);
+            IMongoDatabase db = client.GetDatabase(db_name);
 
-        static async Task gcp_connect()
-        {   
-            var client = new MongoClient("mongodb+srv://username:password@server_ip/test?retryWrites=true&w=majority"); // Change this with the external IP of your primary instance!    D
-            IMongoDatabase db = client.GetDatabase("airbnb"); // Creates the database if it doesn't already exist
 
-            logger.Info("About to query...\n");
             // (collection_name, import_folder, import_chunk_size)
             IList<Tuple<string, string, int>> sources = new List<Tuple<string, string, int>>();
             sources.Add(Tuple.Create("Listings", @"C:/Users/Whyve/Documents/CS488/Dataset/CSV/Listings", 1000));
             sources.Add(Tuple.Create("Reviews", @"C:/Users/Whyve/Documents/CS488/Dataset/CSV/Reviews", 25000));
 
-            IMongoCollection<BsonDocument> collection;
-
-            foreach (Tuple<string, string, int> src in sources)
+            // Import data to db from specified folders
+            if (do_import)
             {
-                collection = db.GetCollection<BsonDocument>(src.Item1);
-                logger.Info($"Importing documets from '{src.Item2}'...");
-                var count = import_folder(src.Item2, collection, src.Item3);
-                logger.Info($"Imported {count.Result.counter} documents from {src.Item2}!");
+                IMongoCollection<BsonDocument> col;
+                foreach (Tuple<string, string, int> src in sources)
+                {
+                    // Set the collection name to the first element in the tuple
+                    col = db.GetCollection<BsonDocument>(src.Item1);
+                    logger.Info($"Importing documets from '{src.Item2}'...");
+
+                    // Import all .json and .csv files from the location specified in the second element of the tuple
+                    // The third element of the tuple specifies how many rows to read in before importing them into the db
+                    var count = import_folder(src.Item2, col, src.Item3);
+                    logger.Info($"Imported {count.Result.counter} documents from {src.Item2}!");
+                }
             }
 
-            collection = db.GetCollection<BsonDocument>(sources[0].Item1);
-            Console.WriteLine("Here are all the one-night stays that cost at most $20:");
-            Console.WriteLine(new string('-', 50));
-            var filter = "{price: {'$lte': 20}, minimum_nights_avg_ntm: 1}"; // Example of how you may query the db
-            await collection.Find(filter)
-                .ForEachAsync(doc => Console.WriteLine(doc));
+            IMongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("Listings");
+
+            // (price_limit, min_nights_limit)
+            List<Tuple<int, int>> test_inputs = new List<Tuple<int, int>>();
+            test_inputs.Add(Tuple.Create(20, 1));
+            test_inputs.Add(Tuple.Create(100, 7));
+            test_inputs.Add(Tuple.Create(700, 31));
+            foreach (Tuple<int, int> input in test_inputs)
+            {
+                Console.WriteLine('\n' + new string('-', 100) + '\n');
+                logger.Debug($"Querying with price_limit={input.Item1} and min_nights_limit={input.Item2}");
+                var count = Test_Query(collection, input.Item1, input.Item2);
+                logger.Info($"Returned {count.Result} records!");
+
+            }
+            
+            // Keep terminal open when program finishes
+            Console.ReadLine();
         }
 
+        // Import all .csv and .json files from the src string into the collection in batches of import_chunk_size
         static async Task<IntHolder> import_folder(string src, IMongoCollection<BsonDocument> collection, int import_chunk_size)
         {
             // Check that src directory exists
@@ -113,10 +133,10 @@ namespace CS488
             return total_count;
         }
 
+        // Read in an entire .json file as a string, turn it into a bson document, and import it all at once into the db
         static async Task<int> import_json(string src, IMongoCollection<BsonDocument> collection)
         {
             string data = System.IO.File.ReadAllText(src);
-            //logger.Debug(data);
             var document = BsonSerializer.Deserialize<BsonDocument>(data);
             int count = document.ElementCount;
             logger.Debug($"Translated {src} into a BSON document with {count} elements!");
@@ -125,6 +145,7 @@ namespace CS488
             return count;
         }
 
+        // Read in rows from a .csv file in a stream
         static async Task<int> import_csv(string src, IMongoCollection<BsonDocument> collection, int import_chunk_size)
         {
             int count = 0;
@@ -134,9 +155,12 @@ namespace CS488
             using (var csv = new CsvReader(fin))
             using (var cr = new CsvDataReader(csv))
             {
-                var headers = new object[cr.FieldCount];
-                var records = new object[cr.FieldCount];
-                logger.Debug($"Parsed {cr.FieldCount} columns in file {src}");
+                // Create utility arrays that can hold all elements of the header row and any given data row
+                int amt_cols = cr.FieldCount;
+                Debug.Assert(amt_cols > 0);
+                var headers = new object[amt_cols];
+                var records = new object[amt_cols];
+                logger.Debug($"Parsed {amt_cols} columns in file {src}");
                 var ret = cr.GetValues(headers);
                 Debug.Assert(ret > 0);
                 logger.Debug($"Returned {ret} : " + string.Join(',', headers));
@@ -149,6 +173,7 @@ namespace CS488
                     Debug.Assert(ret > 0);
 
                     // Create a dictionary mapping each header element to its respective record element
+                    // Weed out any empty string elements
                     var zipped = headers.Zip(records, (h, r) => new { h, r } )
                                         .Where(item => item.r.ToString() != "")
                                         .ToDictionary(item => item.h, item => {
@@ -165,19 +190,62 @@ namespace CS488
                     documents.Add(zipped.ToBsonDocument());
                     ++count;
 
-                    if (count % import_chunk_size == 0) // Insert documents in batches of insert_amt size
+                    // Add documents in batches to the db
+                    if (count % import_chunk_size == 0)
                     {
-                        await collection.InsertManyAsync(documents); // TODO -> use InsertManyAsync for concurrency?
+                        await collection.InsertManyAsync(documents);
                         logger.Debug($"Uploded {import_chunk_size} records to {collection.CollectionNamespace.CollectionName}...");
                         documents.Clear();
                     }
                 }
-                if (documents.Count != 0) // Insert remainder documents
+
+                // Add any remaining docs to the db
+                if (documents.Count != 0)
                 {
                     await collection.InsertManyAsync(documents);
                 }
             }
             return count;
         }     
+
+        // Run a test query on the Listings collection
+        // Queries for all listings of up to price_limit with a min_nights_limit 
+        // Returns the amount of documents from query
+        static async Task<int> Test_Query(IMongoCollection<BsonDocument> collection, int price_limit, int min_nights_limit)
+        {
+            int count = 0;
+
+            Console.WriteLine($"Here are all of the {min_nights_limit}-night stays that cost at most ${price_limit}:");
+            Console.WriteLine(new string('-', 50));
+
+            // Create the query filter (WHERE)
+                                                var b = Builders<BsonDocument>.Filter;
+            var filter = b.Lte("price", price_limit) & b.Eq("minimum_nights_avg_ntm", min_nights_limit);
+            
+            // Create the projection
+            // We can also use a builder here, but string is more consise and managable
+            var projection = "{_id:0, id:1, price:1, neighbourhood_cleansed:1, accomodates:1, smart_location:1, minimum_nights_avg_ntm:1}";
+
+            // Specify sorting
+            SortDefinition<BsonDocument> sort = "{ price: 1 }";
+
+            // Run the query asynchronously
+            await collection.Find(filter)
+                            .Project(projection)
+                            .Sort(sort)
+                            .ForEachAsync(async document =>
+                            {
+                                count++;
+                                var result = document.ToDictionary();
+                                Console.Write($"Book out at listing #{result["id"]} in {result["neighbourhood_cleansed"]} - {result["smart_location"]}");
+                                if (result.ContainsKey("accomodates"))
+                                {
+                                    Console.Write($", which accomodates {result["accomodates"]} people ");
+                                }
+                                Console.WriteLine($" for the lovely {result["minimum_nights_avg_ntm"]} price of ${result["price"]}!");
+                            });
+
+            return count;
+        }
     }
 }
